ce56a880c68b24a2194981369d5611a3d54bba06
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-28 19:33:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments){
   var array = [];
     for (let i = 0; i < musicians.length-1; i++){
-      array.push(`muscians` + `instruments`);
+      array.push(`musicians + plays + instruments`);
     }
 }
